675a32100726ac120b44fdc8df0986298ad2cc2f
Estalin1412 <jose.munoz.d@uni.pe>
2024-12-07 11:02:08 -0500

    Funciones para leer memoria

---
+++ diccionario.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+string diccionario[] = {
+  "estalin",
+  "Estalin",
+   "Aries",
+    "aries",
+    ".",
+    "2203",
+    "220303"
+    "55"
+};
+
+int main() {
+
+
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 #include <windows.h>
+/*Para trabajar con las instancias de procesos*/
+#include <tlhelp32.h>
 
 using std::endl;
 using std::cout;
@@ -15,8 +17,13 @@ using std::string;
 #define OP2CAMBIAR_VALOR_PUNTERO  3
 #define OP3CAMBIAR_PUNTERO_CAMBIANTE  4
 #define OP4TERMINAR_PROCESO  5
+#define OP5PROCESSO_PROCESSO  6
+
+/*Encabezado de funciones*/
 int Menu();
-int Menu(int a);
+int Menu(int &a);
+void funEscanearMemoria(HANDLE gestionar, int targetValue);
+DWORD getProcessID(char * processName);
 
 string diccionary[]={
     "Menu de opciones.",
@@ -27,15 +34,49 @@ string diccionary[]={
     "4) Terminar proceso."
 };
 
-int main (){
+/*DEFINICION DE FUNCIONES------------------------------------------------------------*/
+void ListAllProcesses() {
+    PROCESSENTRY32 entradaProcesos;
+    entradaProcesos.dwSize = sizeof(PROCESSENTRY32);
+    // Tomamos una instantánea de todos los procesos en ejecución
+    HANDLE instancia = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (instancia == INVALID_HANDLE_VALUE) {
+        cout << "No carga instancia" << GetLastError() << endl;
+        return;
+    }
+    // Iteramos por los procesos encontrados
+    if (Process32First(instancia, &entradaProcesos)) {
+        do {
+            cout << "Proceso: " << entradaProcesos.szExeFile
+                 << "ID:  " << entradaProcesos.th32ProcessID << endl;
+        } while (Process32Next(instancia, &entradaProcesos));
+    } else {
+        cout << "Error en la primera entrada" << GetLastError() << endl;
+    }
+    // Cerramos el handle de la instantánea
+    CloseHandle(instancia);
+}
+
+
+int main() {
+    int op;
+    op = Menu(op);
+    ListAllProcesses();
+
+    const char* proceso= "age2.exe";
+
+
 
     return 0;
 }
 
-
-
 /*FUNCIONES-------------------------------------------------------------------------------*/
-//Mostrar menu
+//Función para menu de opciones
+/*
+ * Te muestre el menu de opciones
+ * Te pide que ingrese una opcion
+ * Te devuelve la opcion ingresada
+ */
 int Menu() {
     cout<<diccionary[MENU_DE_OPCIONES]<<endl;
     cout<<diccionary[OP1CAMBIAR_VALOR_COMUN]<<endl;
@@ -43,18 +84,74 @@ int Menu() {
     cout<<diccionary[OP3CAMBIAR_PUNTERO_CAMBIANTE]<<endl;
     cout<<diccionary[OP4TERMINAR_PROCESO]<<endl;
     int a = 0;
+    cout<<"Ingrese la opcion deseada: "<<endl;
     cin>>a;
     return a;
 }
-
-int Menu(int a) {
+/*
+ * Entrada: Ingresa un valor donde quieres guardar el valor
+ * Te muestre el menu de opciones
+ * Te pide que ingrese una opcion
+ * Guarda la opcion ingresada
+ * Te devuelve la opcion ingresada
+ */
+int Menu(int & a){
     cout<<diccionary[MENU_DE_OPCIONES]<<endl;
     cout<<diccionary[OP1CAMBIAR_VALOR_COMUN]<<endl;
     cout<<diccionary[OP2CAMBIAR_VALOR_PUNTERO]<<endl;
     cout<<diccionary[OP3CAMBIAR_PUNTERO_CAMBIANTE]<<endl;
     cout<<diccionary[OP4TERMINAR_PROCESO]<<endl;
+    cout<<"Ingrese la opcion deseada: "<<endl;
     cin>>a;
     return a;
+}
+
+DWORD getProcessID(char * processName) {
+    DWORD procesoId = 0;
+    PROCESSENTRY32 entrada;
+    entrada.dwSize = sizeof(PROCESSENTRY32);
+    HANDLE instancia = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if(Process32First(instancia, &entrada)) {
+        do {
+            if(strcmp(entrada.szExeFile, processName) == 0) {
+                procesoId = entrada.th32ProcessID;
+                break;
+            }
+        } while (Process32Next(instancia, &entrada));
+    }
+    CloseHandle(instancia);
+    return procesoId;
+}
+
+void funEscanearMemoria(HANDLE gestionar, int targetValue) {
+    SYSTEM_INFO inforSistema;
+    GetSystemInfo(&inforSistema);
+
+    LPCVOID inicio = inforSistema.lpMinimumApplicationAddress;
+    LPCVOID fin= inforSistema.lpMaximumApplicationAddress;
+    MEMORY_BASIC_INFORMATION inforMemoria;
+    for(LPCVOID address = inicio; address < fin; ) {
+        if(VirtualQueryEx(gestionar, address,&inforMemoria, sizeof(inforMemoria)) == 0) {
+            break;
+        }
+
+        //Verificamos
+        if(inforMemoria.State == MEM_COMMIT && (inforMemoria.Protect == PAGE_READWRITE || inforMemoria.Protect == PAGE_EXECUTE_READWRITE) ) {
+            BYTE* buffer = new BYTE[inforMemoria.RegionSize];
+            SIZE_T byteLeido;
+
+            if(ReadProcessMemory(gestionar, address, buffer,inforMemoria.RegionSize, &byteLeido)) {
+                for(SIZE_T i = 0; i < byteLeido - sizeof(int); i++) {
+                    int value = *(int*)(buffer+i);
+                    if(value == targetValue) {
+                        cout<<"Se encuentra: "<<value<<" en : "<<static_cast<void*>((BYTE*)address+i)<<endl;
+                    }
+                }
+            }
+            delete[] buffer;
+        }
+        address = (BYTE*)inforMemoria.BaseAddress + inforMemoria.RegionSize;
+    }
 }
 
 
